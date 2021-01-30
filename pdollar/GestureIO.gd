d82d612b941eaf7d1e889644913eadc5112d8387
lmsonic <lorenzomassidda@hotmail.com>
2021-01-30 20:55:42 +0100

    Multiple gestures

--- pdollar/GestureIO.gd
+++ pdollar/GestureIO.gd
@@ -25,35 +25,81 @@ static func save_gesture(gesture:Gesture):
 	for line in gesture.Lines:
 		data.Lines[str(i)]=line._to_json()
 		i+=1
+		
+	var dict := {data.Name: to_json(data)}
 	var file = File.new()
-	file.open(gesture_path, File.WRITE)
-	file.store_line(to_json(data))
+		
+	if file.file_exists(gesture_path):
+		file.open(gesture_path, File.READ)
+		var f = parse_json(file.get_as_text())
+		file.close()
+		f[data.Name]=dict[data.Name]
+		file.open(gesture_path, File.WRITE)
+		file.store_line(to_json(f))
+	else:
+		file.open(gesture_path, File.WRITE)
+		file.store_line(to_json(dict))
+		
+	
 	file.close()
+	
 
 
 
-static func load_gesture(name:String) ->Gesture :
+static func load_gesture(gestureName:String) ->Gesture :
 	var file = File.new()
 	if not file.file_exists(gesture_path):
 		return null
 		
 	file.open(gesture_path, File.READ)
 	var text = file.get_as_text()
+	file.close()
 	var data = parse_json(text)
+	var gestureData = parse_json(data[gestureName])
 	var originalGesturePoints :=[]
 	var lines :=[]
-	for i in range(0,data.OriginalPoints.size()):
-		var pointData= parse_json(data.OriginalPoints[str(i)])
+	for i in range(0,gestureData.OriginalPoints.size()):
+		var pointData= parse_json(gestureData.OriginalPoints[str(i)])
 		var point:Point = Point.new(pointData.X,pointData.Y,pointData.StrokeID)
 		originalGesturePoints.push_back(point)		
-	for i in range(0,data.Lines.size()):
-		var lineData= parse_json(data.Lines[str(i)])
+	for i in range(0,gestureData.Lines.size()):
+		var lineData= parse_json(gestureData.Lines[str(i)])
 		var start:Vector2= Vector2(lineData.start.x,lineData.start.y)
 		var end:Vector2= Vector2(lineData.end.x,lineData.end.y)
 		var color:Color= Color(lineData.color.r,lineData.color.g,lineData.color.b,lineData.color.a)
 		var line:Line = Line.new(start,end,lineData.width,color)
 		lines.push_back(line)
 	
-	var gesture = Gesture.new(originalGesturePoints,lines,data.Name)
-	file.close()
+	var gesture = Gesture.new(originalGesturePoints,lines,gestureName)
+	
 	return gesture
+	
+static func load_gestures() -> Array :
+	var file = File.new()
+	if not file.file_exists(gesture_path):
+		return []
+		
+	file.open(gesture_path, File.READ)
+	var text = file.get_as_text()
+	file.close()
+	var data :Dictionary= parse_json(text)
+	var gestures:=[]
+	for k in data.keys():
+		var gestureData = parse_json(data[k])
+		var originalGesturePoints :=[]
+		var lines :=[]
+		for i in range(0,gestureData.OriginalPoints.size()):
+			var pointData= parse_json(gestureData.OriginalPoints[str(i)])
+			var point:Point = Point.new(pointData.X,pointData.Y,pointData.StrokeID)
+			originalGesturePoints.push_back(point)		
+		for i in range(0,gestureData.Lines.size()):
+			var lineData= parse_json(gestureData.Lines[str(i)])
+			var start:Vector2= Vector2(lineData.start.x,lineData.start.y)
+			var end:Vector2= Vector2(lineData.end.x,lineData.end.y)
+			var color:Color= Color(lineData.color.r,lineData.color.g,lineData.color.b,lineData.color.a)
+			var line:Line = Line.new(start,end,lineData.width,color)
+			lines.push_back(line)
+		
+		var gesture = Gesture.new(originalGesturePoints,lines,k)
+		gestures.push_back(gesture)
+	return gestures
--- pdollar/GestureManager.gd
+++ pdollar/GestureManager.gd
@@ -2,6 +2,7 @@ extends Control
 
 var trainingSet:Array=[]
 signal show_gesture
+signal clear
 const gesture_folder = "user://gestures"
 onready var textEdit := $MarginContainer/HBoxContainer/VBoxContainer/TextEdit
 
@@ -12,8 +13,12 @@ func _on_save():
 		var points = get_parent().points
 		var lines = get_parent().lines
 		GestureIO.save_gesture(Gesture.new(points,lines,name))
+		textEdit.text=""
+		emit_signal("clear")
 	else:
 		print("Name cannot be empty")
+	
+	
 
 func _on_load():
 	var name:String=textEdit.text
@@ -21,6 +26,7 @@ func _on_load():
 		var gesture = GestureIO.load_gesture(name)
 		if gesture:
 			emit_signal('show_gesture',gesture)
+			textEdit.text=""
 		else:
 			print("Could not find gesture")
 
