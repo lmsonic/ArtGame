af9f46344c3bf0484b0d82cffae64668c36178ac
lmsonic <lorenzomassidda@hotmail.com>
2021-01-31 00:28:42 +0100

    character controller fix

--- kinematic character/cubio.gd
+++ kinematic character/cubio.gd
@@ -1,60 +1,34 @@
 extends KinematicBody
 
-const MAX_SPEED = 5
-const JUMP_SPEED = 7
-const ACCELERATION = 2
-const DECELERATION = 4
-const MAX_SLOPE_ANGLE = 30
+var gravity = Vector3.DOWN * 12  # strength of gravity
 
-onready var gravity = -ProjectSettings.get_setting("physics/3d/default_gravity")
-var velocity: Vector3
+var speed = 4  # movement speed
+
+var jump_speed = 6  # jump strength
+
+var spin = 3  # rotation speed
+
+
+var velocity = Vector3()
+var jump = false
+
+func get_input(delta):
+	var vy = velocity.y
+	velocity = Vector3()
+	if Input.is_action_pressed("ui_up"):
+		velocity += -transform.basis.z * speed
+	if Input.is_action_pressed("ui_down"):
+		velocity += transform.basis.z * speed
+	if Input.is_action_pressed("ui_right"):
+		rotate_y(-spin*delta)
+		velocity += transform.basis.x * speed
+	if Input.is_action_pressed("ui_left"):
+		rotate_y(spin*delta)
+		velocity += -transform.basis.x * speed
+	velocity.y = vy
+
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed("exit"):
-		get_tree().quit()
-	if Input.is_action_just_pressed("reset_position"):
-		translation = Vector3(-3, 4, 8)
-
-	var dir = Vector3()
-	dir.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	dir.z = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-
-	# Get the camera's transform basis, but remove the X rotation such
-	# that the Y axis is up and Z is horizontal.
-	var cam_basis = $Target/Camera.global_transform.basis
-	var basis = cam_basis.rotated(cam_basis.x, -cam_basis.get_euler().x)
-	dir = basis.xform(dir)
-
-	# Limit the input to a length of 1. length_squared is faster to check.
-	if dir.length_squared() > 1:
-		dir /= dir.length()
-
-	# Apply gravity.
-	velocity.y += delta * gravity
-
-	# Using only the horizontal velocity, interpolate towards the input.
-	var hvel = velocity
-	hvel.y = 0
-
-	var target = dir * MAX_SPEED
-	var acceleration
-	if dir.dot(hvel) > 0:
-		acceleration = ACCELERATION
-	else:
-		acceleration = DECELERATION
-
-	hvel = hvel.linear_interpolate(target, acceleration * delta)
-
-	# Assign hvel's values back to velocity, and then move.
-	velocity.x = hvel.x
-	velocity.z = hvel.z
+	velocity += gravity * delta
+	get_input(delta)
 	velocity = move_and_slide(velocity, Vector3.UP)
-
-	# Jumping code. is_on_floor() must come after move_and_slide().
-	if is_on_floor() and Input.is_action_pressed("ui_accept"):
-		velocity.y = JUMP_SPEED
-
-
-func _on_tcube_body_entered(body):
-	if body == self:
-		get_node("WinText").show()
