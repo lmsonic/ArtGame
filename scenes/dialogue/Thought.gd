ed6627c2961ca5f396b727fa116e86c00254fb31
lmsonic <lorenzomassidda@hotmail.com>
2021-01-31 09:44:48 +0100

    Dialogue + assets

--- scenes/dialogue/Thought.gd
+++ scenes/dialogue/Thought.gd
@@ -1,10 +1,11 @@
-extends Panel
+extends HBoxContainer
 
 export(Array,Texture) onready var thoughtsTexture 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var children = $MarginContainer/HBoxContainer.get_children()
+	var children = get_children()
 	var i:=0
 	for child in children:
 		if (children.size()>i):
