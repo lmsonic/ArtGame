d699a9c17a26a053c7f19a2cce85f032848eda53
lmsonic <lorenzomassidda@hotmail.com>
2021-01-31 13:03:13 +0100

    Touches

---
+++ scenes/main/Window.gd
@@ -0,0 +1,11 @@
+extends AnimatedSprite3D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func animate(name):
+	play("mad")
