bc4a047bb9d3d5608a07bde1cc850ef19d8123ab
lmsonic <lorenzomassidda@hotmail.com>
2021-02-14 00:22:35 +0100

    all fixed

--- scenes/main/Globals.gd
+++ scenes/main/Globals.gd
@@ -5,7 +5,7 @@ var notebook_collected:=false setget show_commands
 func show_commands(value:bool):
 	notebook_collected = value
 	if value:
-		get_tree().call_group("tutorial","show_commands","press space to open notebook")
+		get_tree().call_group("tutorial","show_commands")
 		get_tree().call_group("bubbles","show_bubbles")
 
 var has_doll:=false
